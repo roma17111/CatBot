58056b2944fdcf95e37aa59aece25d22c69f40e3
Roman Yakimenko <romanze1706@gmail.com>
2024-06-04 14:44:42 +0300

    init project

---
+++ src/main/java/ru/cat/bot/CatBotApplication.java
@@ -0,0 +1,13 @@
+package ru.cat.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatBotApplication.class, args);
+    }
+
+}
